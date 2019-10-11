6e1dca0ed21529407b3b1f2f6e8c13e56565bd6b
Deepi <Deepi@Deepi-PC>
2019-10-11 09:55:44 +0530

    Add these two basepage and homepage

---
+++ src/main/java/Framework/HomePage.java
@@ -0,0 +1,35 @@
+package Framework;
+
+public class HomePage extends BasePage {
+	
+	     @FindBy(xpath =)
+         private WebElement WOMEN;
+	     @FindBy(xpath =)
+         private WebElement WOMEN;
+	     @FindBy(xpath =)
+         private WebElement WOMEN; 
+	     @FindBy(xpath =)
+         private WebElement WOMEN;
+	     @FindBy(xpath =)
+         private WebElement WOMEN;
+	     @FindBy(xpath =)
+         private WebElement WOMEN;
+	     
+public HomePage()  {
+	PageFactory.initElements(driver, this);
+}
+public WebElement WOMEN() {
+return WOMEN;
+}
+public WebElement DRESSES() {
+	DRESSES.click();
+    return DRESSES;
+}
+public WebElement SHIRTS() {
+	
+	
+    return SHIRTS;   
+}
+      public WebElement PRODUc
+
+}
